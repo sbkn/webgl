2e393244dea235551d3d041299e43fd91c3a8d76
appelblim <sergeybakin87@googlemail.com>
2016-05-31 21:05:47 +0200

    added config files and such, WIP

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,37 @@
 var gl;
 
 function start() {
-    var canvas = document.getElementById("glcanvas");
+	var canvas = document.getElementById("glcanvas");
 
-    gl = initWebGL(canvas);
+	gl = initWebGL(canvas);
 
-    if (gl) {
-        gl.viewport(0, 0, canvas.width, canvas.height);
+	if (gl) {
+		gl.viewport(0, 0, canvas.width, canvas.height);
 
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-        gl.enable(gl.DEPTH_TEST);
+		gl.enable(gl.DEPTH_TEST);
 
-        gl.depthFunc(gl.LEQUAL);
+		gl.depthFunc(gl.LEQUAL);
 
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    }
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	}
 }
 
 function initWebGL(canvas) {
-    gl = null;
+	gl = null;
 
-    try {
+	try {
 
-        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    }
-    catch (e) {
-    }
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+	}
+	catch (e) {
+	}
 
-    if (!gl) {
-        alert("Unable to initialize WebGL. Your browser may not support it.");
-        gl = null;
-    }
+	if (!gl) {
+		alert("Unable to initialize WebGL. Your browser may not support it.");
+		gl = null;
+	}
 
-    return gl;
+	return gl;
 }
